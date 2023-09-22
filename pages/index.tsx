afef235670dad23627b8778113651833f625ed94
s.h.m <s.h.mostafavi82@gmail.com>
2023-09-22 15:59:04 +0330

    create navbar for desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import MentorSvg from '../public/images/mentor.svg'
 import { useEffect } from 'react';
+import Navbar from '../components/Navbar/Navbar';
 
 
 
@@ -18,6 +19,7 @@ export default function Home() {
 
   return (
     <div className={styles.Home}>
+      <Navbar></Navbar>
     </div>
   )
 }
