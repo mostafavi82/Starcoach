60d6805bdf2fea4cbf52efa5fcd7457bc65e6dbc
s.h.m <s.h.mostafavi82@gmail.com>
2023-09-25 21:30:05 +0330

    complete part1

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,22 +3,26 @@ import styles from './Navbar.module.scss';
 
 const Navbar = () => {
   return (
-    <div  id={styles.navbar}>
-        <button className={styles.mainBtn}>
-            ورود / ثبت نام
-        </button>
+    <div id={styles.navbar}>
+      <button id={styles.signupBtn} className={styles.mainBtn}>
+        ورود / ثبت نام
+      </button>
 
-        <div id={styles.menuAndLogo}>
+      <div id={styles.menuAndLogo}>
         <button id={styles.menu}>
-            دوره ها
+          دوره ها
         </button>
 
         <button id={styles.menu}>
-            سامانه آموزشی
+          سامانه آموزشی
         </button>
 
-        <img id={styles.logo} src='https://starcoach-files.storage.iran.liara.space/logo.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=342080d4-c2d1-45a3-a670-00ed1438736c%2F20230921%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230921T230900Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=1ebb13cd3f1546c3c1042f962aa0e58c3c57f652d9c8f4771c4abff53a539328'/>
+        <img id={styles.logo} src='https://starcoach-files.storage.iran.liara.space/logo.png' />
         </div>
+
+        <button id={styles.menuIcon}>
+          <img src='https://starcoach-files.storage.iran.liara.space/menu.png'/>
+        </button>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,37 @@ export default function Home() {
   return (
     <div className={styles.Home}>
       <Navbar></Navbar>
+
+      <div>
+        <img id={styles.mainImage} src='https://starcoach-files.storage.iran.liara.space/main-image.png' />
+        <div>
+          <h1 id={styles.mainH1}>
+            برنامه نویسی رو با استارکوچ یاد بگیر
+          </h1>
+
+          <h1 id={styles.mainH1Mobile}>
+          برنامه نویسی رو پروژه محور و با بیشترین بهره وری یاد بگیر
+          </h1>
+
+          <h2 id={styles.mainH2}>
+            یادگیری برنامه نویسی با روش جدید
+          </h2>
+
+          <p id={styles.mainP}>
+         با استارکوچ ، برنامه نویسی رو در کنار دیگران و بصورت پروژه محور یاد بگیر و خودت رو برای ورود به بازار کار آماده کن
+          </p>
+
+          <div id={styles.mainButtons}>
+            <button id={styles.coursesBtn} className={styles.secondBtn}>
+              دوره ها
+            </button>
+            
+            <button className={styles.mainBtn}>
+            از اینجا شروع کن
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
